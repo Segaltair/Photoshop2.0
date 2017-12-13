39a41146bec74b87b50f6c6f53752177db14b30e
Segaltair <ssurakin95@mail.ru>
2017-12-13 10:56:33 +0300

    Поправил 3 лабу

--- IdeaProjects/Savinetskiy_lab1/src/Controller/Coding/InitializeListeners.java
+++ IdeaProjects/Savinetskiy_lab1/src/Controller/Coding/InitializeListeners.java
@@ -2,7 +2,6 @@ package Controller.Coding;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -16,6 +15,8 @@ public class InitializeListeners implements Initializable {
     @FXML
     TextField phraseTextField;
     @FXML
+    TextField code;
+    @FXML
     TextArea tableTextArea;
     @FXML
     TextArea codingTextArea;
@@ -25,20 +26,31 @@ public class InitializeListeners implements Initializable {
     GridPane gridPane;
     @FXML
     Button codingButton;
+    @FXML
+    Button coding;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        lexiconTextField.setText("aeiou!");
-        phraseTextField.setText("eaii!");
+        lexiconTextField.setText("апельсин!");
+        phraseTextField.setText("спаниель!");
+
+        coding.setOnAction(event -> {
+            Encryption encryption = new Encryption(lexiconTextField.getText(), phraseTextField.getText());
+            encryption.coding();
+            //encryption.decoding(encryption.getCoding() ,encryption.getTable(), Double.valueOf(code.getText()));
+
+            tableTextArea.setText(encryption.getTableReply().toString());
+            codingTextArea.setText(encryption.getCodingReply().toString());
+        });
 
         codingButton.setOnAction(event -> {
             Encryption encryption = new Encryption(lexiconTextField.getText(), phraseTextField.getText());
             encryption.coding();
-            encryption.decoding(encryption.getCoding() ,encryption.getTable());
+            encryption.decoding(encryption.getCoding() ,encryption.getTable(), Double.valueOf(code.getText()));
 
             tableTextArea.setText(encryption.getTableReply().toString());
             codingTextArea.setText(encryption.getCodingReply().toString());
-            decodingTextArea.setText(encryption.getDecodingReply().toString());
+            decodingTextArea.setText(encryption.getLogDecoding().toString());
         });
     }
 }
